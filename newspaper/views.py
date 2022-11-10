822a983e4b592c29fedca2e7054667efe9785861
Kateryna Kilyk <katekilik@gmail.com>
2022-11-10 12:15:29 +0200

    Added index funcion to views and templates

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Redactor, Newspaper, Topic
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "newspaper/index.html", context=context)
